1db3c659cbe8818ec23c7e095f1ca05ca8397b9e
rpw1 <47835461+rpw1@users.noreply.github.com>
2025-12-29 16:57:42 -0500

    removing environment file (#94)

--- woozle-web/src/environments/environment.ts
+++
@@ -1,6 +0,0 @@
-export const environment = {
-  production: false,
-  spotifyClientId: '0b21d2bcc96348d7a85e8ae44eed0127',
-  woozleApiBaseUrl: 'http://localhost:5151',
-  baseUrl: 'http://127.0.0.1:4200',
-};
