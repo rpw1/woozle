cf73197b446b6566dff3bc479c8ac4d5d5d7344f
rpw1 <47835461+rpw1@users.noreply.github.com>
2025-12-28 21:14:19 -0500

    fixing issues when using linux (#91)
    * fixing issues when using linux
    
    * fix prettier cli

--- woozle-web/src/environments/environment.ts
+++ woozle-web/src/environments/environment.ts
@@ -1,6 +1,6 @@
 export const environment = {
   production: false,
-  spotifyClientId: '0b21d2bcc96348d7a85e8ae44eed0127',
-  woozleApiBaseUrl: 'https://localhost:7178',
-  baseUrl: 'http://[::1]:4200',
+  spotifyClientId: '{{Client_Id}}',
+  woozleApiBaseUrl: 'http://localhost:5151',
+  baseUrl: 'http://127.0.0.1:4200',
 };
